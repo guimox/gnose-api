62eeb5371d32035d0d53ee53b23498666921d067
gxlpes <guilhermxlopes@gmail.com>
2024-10-24 22:41:09 -0300

    chore: trying to deploy it to vps

--- src/main/java/com/gnose/api/GnoseApplication.java
+++ src/main/java/com/gnose/api/GnoseApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableAutoConfiguration
 @RestController
-@RequestMapping("/api")
 public class GnoseApplication {
 
 	@GetMapping
