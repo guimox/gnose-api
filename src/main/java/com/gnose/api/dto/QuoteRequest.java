de4de52c69c12ab0f6a2d604d9ebea0e48a6880c
gxlpes <guilhermxlopes@gmail.com>
2024-11-05 07:57:41 -0300

    feat: added dto

---
+++ src/main/java/com/gnose/api/dto/QuoteRequest.java
@@ -0,0 +1,13 @@
+package com.gnose.api.dto;
+
+public class QuoteRequest {
+    private String quote;
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
